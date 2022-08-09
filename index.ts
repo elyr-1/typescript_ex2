b7f63f43eb3c5a757a2b8b8d3a7a390f373b7d58
elyrick <elyrick.manlagnit@gmail.com>
2022-08-09 09:25:10 +0800

    Exercise 2

---
+++ index.ts
@@ -0,0 +1,33 @@
+export const isbnTag = (isbn: string) => { // takes a string as an argument and returns a boolean value
+    if (isbn.length != 10) { // if length is not 10, ISBN is invalid & returns false
+        console.log(isbn);
+        return false;
+    }
+    else { // if length is 10
+        let sum = 0; // initialize the sum
+        for (let i = 0; i < isbn.length - 1; i++) { // loop from the 1st to 9th digit
+            let num = parseInt(isbn[i]) * (i + 1); // parse each element and multiply by (i + 1)
+            sum += num; // compute the sum of the first 9 digits
+        }
+
+        if (isbn.charAt(isbn.length - 1) === "X") { // check if the last digit is X
+            sum += 100; // add 10 * 10 = 100 to the sum
+        } else {
+            sum += parseInt(isbn.charAt(isbn.length - 1)) * (isbn.length);
+        }
+
+        console.log(isbn); // print the ISBN tag 
+
+        if (sum % 11 == 0) { // check if the sum is divisible by 11
+            return true;
+        } else {
+            return false;
+        }
+    }
+ 
+}
+
+console.log(isbnTag("1112223339")); // this is a valid ISBN
+console.log(isbnTag("123456789X")); // this is a valid ISBN
+console.log(isbnTag("0111111111")); // this is an invalid ISBN
+console.log(isbnTag("X123456789")); // this is an invalid ISBN
